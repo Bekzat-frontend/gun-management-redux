541e64e6b6ca06c950ecb6d26f8e25512b63950c
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-03-19 03:07:47 +0700

    gunAction create

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
     <div>
-      
+      <AppRouter />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
